2dd09c17b0fcf47769dd69d48425d783959f77ac
Rafael Chavez Solis <rcsolis@Rafaels-MacBook-Pro.local>
2023-03-24 17:09:55 -0600

    Divide code into library and binary crate

--- src/lib.rs
+++ src/lib.rs
@@ -24,12 +24,71 @@ fn read_file_contents(file_path: &str)-> Result<String, io::Error>{
     // Read file contents
     fs::read_to_string(file_path)
 }
+// Function to search for a query string in a file contents
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+
+    let mut result: Vec<&str> = Vec::new();
+
+    for line in contents.lines(){
+        if line.contains(query){
+            result.push(line);
+        }
+    }
+
+    return result;
+}
+
 // Public function to run the program logic
 pub fn run (config: Config)-> Result<(), Box<dyn error::Error>>{
     println!("Searching for: {} in file: {}", config.query_string, config.file_path);
     // Read file contents
     let file_contents = read_file_contents(&config.file_path)?;
-    println!("File contents: {}", file_contents);
-    
+    // println!("File contents: \n {}", file_contents);
+    // Search for query string in file contents
+    let results = search(&config.query_string, &file_contents);
+    if results.len()==0{
+        return Err("Query string not found, try again".into());
+    }
+    // Print results
+    println!("\nQuery string found! \nTotal matches:{}", results.len());
+    println!("Found in lines: ");
+    for result in results{
+        println!("->{}", result);
+    }
     Ok(())
+}
+
+// Create tests module
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe,fast,productive.
+Pick three.
+I'm want to be a Rustacean!";
+        
+        assert_eq!(
+            vec!["safe,fast,productive."],
+            search(query, contents)
+        );
+    }
+    #[test]
+    fn multiple_results(){
+        let query = "Rust";
+        let contents = "\
+Rust:
+safe,fast,productive.
+Pick three.
+I'm want to be a Rustacean!";
+        
+        assert_eq!(
+            vec!["Rust:", "I'm want to be a Rustacean!"],
+            search(query, contents)
+        );
+    }
 }
